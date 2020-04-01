d0f84b006df5be422897f8b2ccfd0d1c4ceacf2e
Dominique Kevin Macasero <kevinmacasero123@gmail.com>
2020-04-01 20:19:49 +0800

    Source Service Change

--- src/app/services/source/source.service.ts
+++ src/app/services/source/source.service.ts
@@ -5,19 +5,21 @@ import { HTTP, HTTPResponse } from "@ionic-native/http/ngx";
 import { SourceModel } from "src/app/models/source/source.model";
 import { Globals } from "src/app/utils/globals/globals.util";
 
-import { Observable, from } from "rxjs";
-import { map } from "rxjs/operators";
+import { Observable, from, of } from "rxjs";
+import { map, find } from "rxjs/operators";
+import { HeaderUtil } from "src/app/utils/header/header.util";
 
 @Injectable({ providedIn: "root" })
 export class SourceService {
   sourceEndpoint =
     "sources/v0.jsonld?types=SensorSystem&country=NO&municipality=HALDEN";
 
-  constructor(private http: HTTP) {}
+  constructor(private http: HTTP, private headerUtil: HeaderUtil) {}
 
-  getSource(headers: object) {
+  getSource() {
+    const header = this.headerUtil.getHeader(Globals.clientID);
     return from(
-      this.http.get(Globals.APIURL + this.sourceEndpoint, null, headers)
+      this.http.get(Globals.APIURL + this.sourceEndpoint, null, header)
     ).pipe(
       map((sources: any) => {
         return JSON.parse(sources.data);
@@ -29,4 +31,16 @@ export class SourceService {
       })
     );
   }
+
+  getSourceById(id: string): Observable<SourceModel> {
+    return this.getSource().pipe(
+      map((sources: SourceModel[]) =>
+        sources.find((source: SourceModel) => source.id == id)
+      )
+    );
+  }
 }
+// https://frost.met.no/observations/v0.jsonld?sources=SN18700&referenc
+// etime=2019-11-25/2019-11-
+// 25&elements=air_temperature,wind_speed,boolean_fair_weather(clou
+// d_area_fraction P1D)
